82a207e87aa18098ac2a2c010fc25cb53c452e91
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2025-08-16 16:35:39 +0200

    1 - add ViewActor and manager states, setup message exchange

--- src/main/scala/pcd/ass03/BoidActor.scala
+++ src/main/scala/pcd/ass03/BoidActor.scala
@@ -1,15 +1,19 @@
 package pcd.ass03
 
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
 
 trait Update
 final case class UpdateVel(from: ActorRef[UpdatedVel]) extends Update
 final case class UpdatePos(from: ActorRef[UpdatedPos]) extends Update
+final case class UpdateView(from: ActorRef[UpdatedView]) extends Update
 
 trait Updated
 final case class UpdatedVel() extends Updated
 final case class UpdatedPos() extends Updated
+final case class UpdatedView() extends Updated
+
+val NumBoids = 10
 
 object BoidActor:
   def apply(): Behavior[Update] = Behaviors.receive: (context, message) =>
@@ -23,33 +27,60 @@ object BoidActor:
         from ! UpdatedPos()
         Behaviors.same
 
+object ViewActor:
+  def apply(): Behavior[Update] = Behaviors.receive: (context, message) =>
+    message match
+      case UpdateView(from) =>
+        context.log.info(s"${context.self}: Updating view, from $from")
+        from ! UpdatedView()
+        Behaviors.same
+
 object BoidsManager:
   def apply(): Behavior[Updated] = Behaviors.setup:
     context =>
-      val boidActor = context.spawn(BoidActor(), "boidActor")
-      boidActor ! UpdateVel(context.self)
-      Behaviors.same
+      var boidActors: List[ActorRef[Update]] = List.empty
+      for
+        i <- 0 until NumBoids
+      yield boidActors = context.spawnAnonymous(BoidActor()) +: boidActors
+      boidActors.foreach(_ ! UpdateVel(context.self))
+      new BoidsManager(context, boidActors).waitingVel
 
-  /*def apply(): Behavior[Updated] = Behaviors.receive: (context, message) =>
-    message match
-      case UpdatedVel() =>
-        context.log.info("Updating vel")
-        from ! UpdatedVel()
-        Behaviors.same
-      case UpdatedPos() =>
-        context.log.info("Updating pos")
-        from ! UpdatedPos()
-        Behaviors.same*/
+class BoidsManager(ctx: ActorContext[Updated], boids: List[ActorRef[Update]]):
+  private var _counter = 0
+  val viewActor: ActorRef[Update] = ctx.spawnAnonymous(ViewActor())
 
-class BoidsManager
-  /*val waiting: Behavior[Command] =
+  val waitingVel: Behavior[Updated] =
     Behaviors.receiveMessagePartial:
-      case Think =>
-        ctx.log.info("{} starts to think", name)
-        startThinking(ctx, 5.seconds)*/
+      case UpdatedVel() =>
+        _counter += 1
+        ctx.log.info(s"BoidActor #$_counter has updated velocity")
+        if _counter == NumBoids then
+          _counter = 0
+          boids.foreach(_ ! UpdatePos(ctx.self))
+          waitingPos
+        else
+          Behaviors.same
+
+  val waitingPos: Behavior[Updated] =
+    Behaviors.receiveMessagePartial:
+      case UpdatedPos() =>
+        _counter += 1
+        ctx.log.info(s"BoidActor #$_counter has updated position")
+        if _counter == NumBoids then
+          _counter = 0
+          viewActor ! UpdateView(ctx.self)
+          waitingView
+        else
+          Behaviors.same
+
+  val waitingView: Behavior[Updated] =
+    Behaviors.receiveMessagePartial:
+      case UpdatedView() =>
+        _counter += 1
+        ctx.log.info(s"ViewActor #$_counter has updated view")
+        Behaviors.same
 
 object BoidMain extends App:
   val system: ActorSystem[Updated] = ActorSystem(BoidsManager(), name = "boid-manager")
-  //system ! HelloActor.Greet("Akka Typed", system.ignoreRef)
   Thread.sleep(5000)
   system.terminate()
