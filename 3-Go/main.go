0c95760e559d99dc1b203677a3ba91983b715dc1
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2025-09-03 10:16:38 +0200

    3 - add player score

--- 3-Go/main.go
+++ 3-Go/main.go
@@ -22,23 +22,37 @@ var moveName = map[Move]string{
 	Rock:     "rock",
 }
 
-var result = map[[2]Move]string{
-	{Rock, Scissors}:     "win",
-	{Scissors, Paper}:    "win",
-	{Paper, Rock}:        "win",
-	{Scissors, Rock}:     "lose",
-	{Paper, Scissors}:    "lose",
-	{Rock, Paper}:        "lose",
-	{Rock, Rock}:         "draw",
-	{Scissors, Scissors}: "draw",
-	{Paper, Paper}:       "draw",
+type Result int
+
+const (
+	Win Result = iota
+	Lose
+	Draw
+)
+
+var resultName = map[Result]string{
+	Win:  "win",
+	Lose: "lose",
+	Draw: "draw",
+}
+
+var result = map[[2]Move]Result{
+	{Rock, Scissors}:     Win,
+	{Scissors, Paper}:    Win,
+	{Paper, Rock}:        Win,
+	{Scissors, Rock}:     Lose,
+	{Paper, Scissors}:    Lose,
+	{Rock, Paper}:        Lose,
+	{Rock, Rock}:         Draw,
+	{Scissors, Scissors}: Draw,
+	{Paper, Paper}:       Draw,
 }
 
 func main() {
 	moveChannel1 := make(chan Move)
 	moveChannel2 := make(chan Move)
-	resultChannel1 := make(chan string)
-	resultChannel2 := make(chan string)
+	resultChannel1 := make(chan Result)
+	resultChannel2 := make(chan Result)
 	go referee("referee", moveChannel1, moveChannel2, resultChannel1, resultChannel2)
 	go player("p1", moveChannel1, resultChannel1)
 	go player("p2", moveChannel2, resultChannel2)
@@ -47,8 +61,8 @@ func main() {
 	}
 }
 
-func referee(name string, moveChannel1 chan Move, moveChannel2 chan Move, resultChannel1 chan string,
-	resultChannel2 chan string) {
+func referee(name string, moveChannel1 chan Move, moveChannel2 chan Move, resultChannel1 chan Result,
+	resultChannel2 chan Result) {
 	for {
 		moveP1, moveP2 := <-moveChannel1, <-moveChannel2
 		//fmt.Println(name, moveName[moveP1], moveName[moveP2])
@@ -58,11 +72,16 @@ func referee(name string, moveChannel1 chan Move, moveChannel2 chan Move, result
 	}
 }
 
-func player(name string, moveChannel chan Move, resultChannel chan string) {
+func player(name string, moveChannel chan Move, resultChannel chan Result) {
+	var score = 0
 	for {
 		move := Move(rand.Intn(3))
 		moveChannel <- move
 		result := <-resultChannel
-		fmt.Println(name, moveName[move], result)
+		if result == Win {
+			score++
+		}
+		fmt.Println(name, moveName[move], resultName[result])
+		fmt.Println(name, score)
 	}
 }
